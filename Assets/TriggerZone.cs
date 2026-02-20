3e4587134e7da04c86f442f37b67edfd05eae3f1
dianalifeliver <ddianka1@gmail.com>
2026-02-20 11:54:16 +0000

    Third change

---
+++ Assets/TriggerZone.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log ("Player Entered");
+            gameObject.SetActive(false);
+        }
+    }
+}
